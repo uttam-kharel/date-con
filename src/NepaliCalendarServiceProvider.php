f8bca3a35de0d63207b973f987c1b81b170b32d9
TheSickness <imuttamkharel@gmail.com>
2026-08-14 12:30:00 +0545

    chore: bump version to 1.10.0

--- src/NepaliCalendarServiceProvider.php
+++ src/NepaliCalendarServiceProvider.php
@@ -20,7 +20,7 @@ use Sambat\NepaliCalendar\Query\NepaliDateQueryBuilder;
 
 class NepaliCalendarServiceProvider extends ServiceProvider
 {
-    public const VERSION = '1.9.0';
+    public const VERSION = '1.10.0';
 
     public function register(): void
     {
